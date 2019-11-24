fc8de157bbe3f3e826305e6f0e97ff3bc05c5d99
Eunsu <dmstndlekd@gmail.com>
2019-11-24 19:21:58 +0900

    update

--- last_project/settings.py
+++ last_project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'acoounts',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 AUTH_USER_MODEL = 'accounts.User'
