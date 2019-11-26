bf4853a4f9b10b2930442665a9ea557613ab0e2b
Eunsu <dmstndlekd@gmail.com>
2019-11-26 10:41:52 +0900

    final_movie.json

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+import json
 User = get_user_model()
 
 class Genre(models.Model):
-    # genre_id = models.IntegerField(null=True)
     name = models.CharField(max_length=100, blank=True)
 
 
@@ -23,6 +23,18 @@ class Movie(models.Model):
     # def import_data(cls):
     #     with open('./movies/fixtures/movie.json', 'r', encoding='utf-8') as f:
     #         movies = json.load(f)
+    #         for date in range(1980, 2020):
+    #             conn = http.client.HTTPSConnection("api.themoviedb.org") 
+    #             payload = "{}"
+    #             date_api = "/3/discover/movie?primary_release_year={}&page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=ko-KR&api_key=c7071549de08bd22fd6ecb4d67cf2099".format(date)
+    #             image_api = "https://image.tmdb.org/t/p/w300_and_h450_bestv2"
+    #             conn.request("GET", date_api, payload)
+    #             res = conn.getresponse()
+    #             data = res.read()
+    #             movie_data = json.loads(data.decode("utf-8"))
+    #             result += movie_data['results'][:10]
+    #         for i in range(len(movies)):
+    #             movies[i]['fields']['genre']=result[i]['genre_ids']
     #         for movie in movies:
     #             genres_in_movie = movie['fields']
     #             m = cls.objects.create(                    
@@ -32,6 +44,8 @@ class Movie(models.Model):
     #                 poster_url = movie['poster_path'],
     #                 release_date = movie['release_date'],
     #                 genre = movie['genre_ids']
+    #             with open('movies.json', 'w', encoding='utf-8') as f:
+    #                 f.write(json.dump(movies))
     #                 # genres = ','.join(gl)
     #                 # watch_grade = movie['audits'],
     #             )
@@ -46,5 +60,4 @@ class Review(models.Model):
     content = models.CharField(max_length=100)
     rate = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
 
